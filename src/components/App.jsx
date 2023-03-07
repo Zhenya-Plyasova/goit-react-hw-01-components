1647845dbb3dab3bf35560e78b65dfa0678f8f0b
Zhenya Plyasova <111362107+Zhenya-Plyasova@users.noreply.github.com>
2023-03-07 19:56:01 +0200

    tasks_resolved

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
+import user from '../user.json';
+import data from '../data.json';
+import friends from '../friends.json';
+import transactions from '../transactions.json';
+import { FriendList } from './FriendList/FriendList';
+import { Profile } from "./Profile/Profile";
+import { Statistics } from './Statistics/Statistics';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
-        display: 'flex',
+        display: 'block',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
         color: '#010101'
       }}
     >
-      React homework template
+      <Profile 
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}/>
+
+      <Statistics title="Upload stats" stats={data} />
+
+      <FriendList friends={friends} />
+
+      <TransactionHistory items={transactions}/>
+     
     </div>
+
   );
 };
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,43 @@
+import css from './Profile.module.css'
+import PropTypes from "prop-types";
+const defaultImage = "https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+
+export const Profile = ({
+    username,tag,location,avatar = defaultImage,stats, 
+})=>{
+  return <div className={css.profile}>
+    <div className={css.description}>
+      <img
+        src={avatar}
+        alt="User avatar"
+        className={css.avatar}
+      />
+      <p className={css.name}>{username}</p>
+      <p className={css.tag}>@{tag}</p>
+      <p className={css.location}>{location}</p>
+    </div>
+  
+    <ul className={css.stats}>
+      <li className = {css.statsItem}>
+        <span className={css.label}>Followers</span>
+        <span className={css.quantity}>{stats.followers}</span>
+      </li>
+      <li className = {css.statsItem}>
+        <span className={css.label}>Views</span>
+        <span className={css.quantity}>{stats.views}</span>
+      </li>
+      <li className = {css.statsItem}>
+        <span className={css.label}>Likes</span>
+        <span className={css.quantity}>{stats.likes}</span>
+      </li>
+    </ul>
+  </div>
+};
+
+Profile.propTypes = {
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  avatar: PropTypes.string,
+  stats: PropTypes.objectOf(PropTypes.number)
+}
